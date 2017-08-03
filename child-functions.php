d4b8c4125cccd90a8b686733210378b36783b98d
Luke DeWitt <luke.dewitt@redspace.com>
2017-08-03 10:27:04 -0300

    working on release implementation for themes

--- child-functions.php
+++ child-functions.php
@@ -1,15 +1,7 @@
 <?php
+// todo: may be worth moving this to wp-config.php
+// todo: update these values as needed
+$github_access_token = false;
+$github_repo_name = 'poc-understrap-child';
+
 /* child specific logic can live here */
-
-function theme_enqueue_styles() {
-	// Get the theme data
-	$the_theme = wp_get_theme();
-  $theme_name = get_stylesheet(); // because child theme
-
-  // styles
-  wp_enqueue_style( "{$theme_name}-styles", get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) ); // TODO: update child theme file name
-  
-  // scripts
-  wp_enqueue_script( "{$theme_name}-scripts", get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true ); // TODO: update child theme file name
-}
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
--- functions.php
+++ functions.php
@@ -27,29 +27,91 @@ function understrap_remove_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'understrap_remove_scripts', 20 );
 
-// this will simply remove the "update now" option on a theme
-// this will force even administrators to follow a proper flow
-// for updating themes.
-function theme_pre_set_transient_update_theme($transient) {
-	$theme_name = get_stylesheet(); // because child theme...
-
-	if( empty( $transient->checked[$theme_name] ) ) {
-		return $transient;
-	}
-
-	// TODO: Check releases versus github...
-
-	$transient->response[$theme_name] = false;
-
-	return $transient;
-}
-add_filter( 'pre_set_site_transient_update_themes', 'theme_pre_set_transient_update_theme' );
-
 // echo out a message in the footer so we can ensure the site is running
 function ensure_site_status() {
     echo "<!--STATUS:OK-->";
 }
 add_action( 'wp_footer', 'ensure_site_status' );
 
+function theme_enqueue_styles() {
+	// Get the theme data
+	$the_theme = wp_get_theme();
+  $theme_name = get_stylesheet(); // because child theme
+
+  // styles
+  wp_enqueue_style( "{$theme_name}-styles", get_stylesheet_directory_uri() . '/css/child-theme.min.css', array(), $the_theme->get( 'Version' ) ); // TODO: update child theme file name
+  
+  // scripts
+  wp_enqueue_script( "{$theme_name}-scripts", get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true ); // TODO: update child theme file name
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+// todo: ensure all the "required" plugins are activated (cleaner than mu-plugins)
+
 // child theme specific logic
 require_once('child-functions.php');
+
+// at the end of the day, this may be worth moving to an mu-plugin to avoid folks from potentially breaking things
+
+// this is where the release info will be stored...
+$api_result = false;
+
+// get the release info
+function get_repo_release_info() {
+	global $github_access_token, $github_repo_name;
+
+	// Only do this once
+	if ( !empty( $api_result ) ) {
+		return;
+	}
+
+	// Query the GitHub API
+	$url = "https://api.github.com/repos/whatadewitt/{$github_repo_name}/releases";
+
+	// We need the access token for private repos
+	if ( !empty( $github_access_token ) ) {
+		$url = add_query_arg( array( 'access_token' => $github_access_token ), $url );
+	}
+
+	// Get the results
+	$api_result = wp_remote_retrieve_body( wp_remote_get( $url ) );
+	if ( !empty( $api_result ) ) {
+		$api_result = @json_decode( $api_result );
+	}
+
+	// Use only the latest release
+	if ( is_array( $api_result ) ) {
+		$api_result = $api_result[0];
+	}
+}
+
+// this will simply remove the "update now" option on a theme
+// this will force even administrators to follow a proper flow
+// for updating themes.
+function theme_pre_set_transient_update_theme( $transient ) {
+	$theme_name = get_stylesheet(); // because child theme...
+	error_log("HIT");
+
+	// if( empty( $transient->checked[$theme_name] ) ) {
+	// 	return $transient;
+	// }
+
+	if ( empty( $transient->checked ) ) {
+		return $transient;
+	}
+
+	get_repo_release_info();
+	$do_update = version_compare( $api_result->tag_name, $transient->checked[$theme_name] );
+
+	if ( $do_update == 1 ) {
+		$obj = new stdClass();
+		$obj->slug = $theme_name;
+		$obj->new_version = $this->api_result->tag_name;
+		$obj->url = $this->plugin_data['PluginURI'];
+		$obj->package = false;
+		$transient->response[$theme_name] = $obj;
+	}
+
+	return $transient;
+}
+add_filter( 'pre_set_site_transient_update_themes', 'theme_pre_set_transient_update_theme' );
